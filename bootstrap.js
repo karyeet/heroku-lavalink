788c457547586f351ba80ae5d1ac47aaf4cbb863
KareemRS <kareem.shehada@gmail.com>
2020-04-03 22:48:56 -0700

    Update bootstrap.js

--- bootstrap.js
+++ bootstrap.js
@@ -1,16 +1,32 @@
+  
 const fs = require('fs')
 
 let application = fs.readFileSync('./application.yml','utf8')
-application = application.replace('DYNAMICPORT-CHANGEME',process.env.PORT)
+
+if(process.env.PORT){
+	application = application.replace('DYNAMICPORT',process.env.PORT)
+}
+
 if(process.env.PASS){
 	application = application.replace('youshallnotpass',process.env.PASS)
 }
 fs.writeFileSync('./application.yml', application)
 
-const exec = require('child_process').exec;
-const childProcess = exec('java -jar Lavalink.jar', function(err, stdout, stderr) {
-    if (err) {
-        console.log(err)
-    }
-    console.log(stdout)
-})
+const spawn = require('child_process').spawn;
+const child = spawn('java', ['-jar', 'Lavalink.jar'])
+
+child.stdout.on('data',(data)=>{
+	console.log(`stdout: ${data}`);
+});
+
+child.stderr.on('data', (data) => {
+  console.error(`stderr: ${data}`);
+});
+
+child.on('error',(error)=>{
+	console.error(error);
+});
+
+child.on('close', (code) => {
+  console.log(`Lavalink exited with code ${code}`);
+});
