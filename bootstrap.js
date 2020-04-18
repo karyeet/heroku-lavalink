e89bffe0b32d6325ef1990ed46e720665c69645e
KareemRS <kareem.shehada@gmail.com>
2020-04-18 16:00:18 -0700

    http https mistake

--- bootstrap.js
+++ bootstrap.js
@@ -1,53 +1,55 @@
-  
+
 const fs = require('fs')
 const http = require('http')
-let application = fs.readFileSync('./application.yml','utf8')
+let application = fs.readFileSync('./application.yml', 'utf8')
 
-if(process.env.PORT){
-	application = application.replace('DYNAMICPORT',process.env.PORT)
+if (process.env.PORT) {
+    application = application.replace('DYNAMICPORT', process.env.PORT)
 }
 
-if(process.env.PASS){
-	application = application.replace('youshallnotpass',process.env.PASS)
+if (process.env.PASS) {
+    application = application.replace('youshallnotpass', process.env.PASS)
 }
 fs.writeFileSync('./application.yml', application)
 
-const download = function(url, dest, cb) { //modified code from https://stackoverflow.com/a/22907134
-  var file = fs.createWriteStream(dest);
-  var request = http.get(url, function(response) {
-    response.pipe(file);
-    file.on('finish', function() {
-      file.close(cb);
+const download = function (url, dest, cb) { //modified code from https://stackoverflow.com/a/22907134
+    const file = fs.createWriteStream(dest);
+    const request = http.get(url, function (response) {
+        response.pipe(file);
+        console.log('Downloading Lavalink.jar')
+        file.on('finish', function () {
+            console.log('Downloaded Lavalink.jar')
+            file.close(cb);
+        });
+    }).on('error', function (err) {
+        fs.unlinkSync(dest);
+        console.error(err)
     });
-  }).on('error', function(err) {
-    fs.unlinkSync(dest);
-	console.error(err)
-  });
 };
 
-function startLavalink(){
-const spawn = require('child_process').spawn;
-const child = spawn('java', ['-jar', 'Lavalink.jar'])
+function startLavalink() {
+    const spawn = require('child_process').spawn;
+    const child = spawn('java', ['-jar', 'Lavalink.jar'])
 
-child.stdout.setEncoding('utf8')
-child.stderr.setEncoding('utf8')
+    child.stdout.setEncoding('utf8')
+    child.stderr.setEncoding('utf8')
 
-child.stdout.on('data',(data)=>{
-	console.log(data);
-});
+    child.stdout.on('data', (data) => {
+        console.log(data);
+    });
 
-child.stderr.on('data', (data) => {
-  console.error(data);
-});
+    child.stderr.on('data', (data) => {
+        console.error(data);
+    });
 
-child.on('error',(error)=>{
-	console.error(error);
-});
+    child.on('error', (error) => {
+        console.error(error);
+    });
 
-child.on('close', (code) => {
-  console.log(`Lavalink exited with code ${code}`);
-});
+    child.on('close', (code) => {
+        console.log(`Lavalink exited with code ${code}`);
+    });
 }
 
-const cdn = 'https://cdn.glitch.com/77b0e0bb-e744-442a-bbbd-03c3fe832534%2FLavalink.jar?v=1587249367105'
-download(cdn,'./Lavalink.jar',startLavalink)
+const cdn = 'http://cdn.glitch.com/77b0e0bb-e744-442a-bbbd-03c3fe832534%2FLavalink.jar?v=1587249367105'
+download(cdn, './Lavalink.jar', startLavalink)
